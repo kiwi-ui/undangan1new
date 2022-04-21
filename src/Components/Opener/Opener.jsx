2ec3d394178f520616727b3df4155e2f9ade4a25
kiwi-ui <yusnianggara@gmail.com>
2022-04-21 17:09:21 +0700

    sudah responsive headernya

--- src/Components/Opener/Opener.jsx
+++ src/Components/Opener/Opener.jsx
@@ -8,14 +8,15 @@ const Opener = () => {
       <section className="opener z-0 text-center mx-auto h-screen bg-hero bg-no-repeat bg-cover bg-center md:bg-contain sm:bg-contain overflow-x-hidden overflow-y-scroll">
           <div className="filteropener bg-emerald-700/30 z-1 h-screen bg-center w-screen fixed">
           </div>
-          <div className='containeropener mt-44 mx-auto flex flex-col justify-center items-center gap-y-5 w-screen h-screen fixed '>
+          <div className='containeropener justify-content-center mt-44 mx-auto flex flex-col justify-center items-center gap-y-5 w-screen h-screen fixed '>
 
               {/* <img className='w-16 md:w-32 lg:w-48 object-center    ' src={Img1} alt="" /> */}
               <div>
-                  <h3 className='text-white font-bold text-xl'>Kepada yang terhormat</h3>
-                  <h3 className='text-white font-bold text-xl'>Saudara/saudari</h3>
-                  <h4 className='judulopener text-5xl text-center text-orange-300 font-Merriweather font-bold my-5'>Dummy</h4>
-                  <h3 className='text-white font-bold m-3 text-xl'>Pada acara pernikahan kami</h3>
+                  <h3 className='text-white font-bold md:text-xl text-sm'>Kepada yang terhormat</h3>
+                  <h3 className='text-white font-bold md:text-xl text-sm'>Saudara/saudari</h3>
+                  <h4 className='judulopener md:text-5xl text-center text-orange-300 font-Merriweather font-bold my-5 text-xl'>Dummy</h4>
+                  <h3 className='text-white font-bold m-3 md:text-xl text-sm'>Kami mengundang saudara/saudari pada</h3>
+                  <h3 className='text-white font-bold m-3 md:text-xl text-sm'>acara pernikahan kami</h3>
               </div>
               <div>
                   <button className='btnopener bg-orange-300/70 p-3 rounded-2xl text-gray-100 font-medium w-56 inline-flex justify-center gap-4 '>
